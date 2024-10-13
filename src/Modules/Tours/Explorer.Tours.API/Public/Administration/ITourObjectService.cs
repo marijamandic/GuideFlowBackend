f226ebf02e77bf3a9ee322e5bcea0406686934f3
Nikola Ratkovac <nikolaratkovac3@gmail.com>
2024-10-13 18:49:28 +0200

    feat: added TourObject class, Dto and service

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/Administration/ITourObjectService.cs
@@ -0,0 +1,8 @@
+﻿using Explorer.Tours.API.Dtos;
+using FluentResults;
+
+namespace Explorer.Tours.API.Public.Administration;
+public interface ITourObjectService
+{
+    Result<TourObjectDto> Create(TourObjectDto tourObject);
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourObject.cs
@@ -0,0 +1,29 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain;
+
+public enum ObjectCategory
+{
+    Parking,
+    Restaurant,
+    Toilet,
+    Other
+}
+
+public class TourObject : Entity
+{
+    public string Name { get; init; }
+    public string? Description { get; init; }
+    public string ImageUrl { get; init; }
+    public ObjectCategory Category { get; init; }
+
+    public TourObject(string name, string? description, string imageUrl, ObjectCategory category)
+    {
+        if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Invalid Name.");
+        if (string.IsNullOrWhiteSpace(imageUrl)) throw new ArgumentException("Invalid Image path.");
+        Name = name;
+        Description = description;
+        ImageUrl = imageUrl;
+        Category = category;
+    }
+}
