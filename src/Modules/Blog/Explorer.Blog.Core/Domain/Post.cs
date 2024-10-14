cf5b6fbbd85704fec73ab428afdbc597baf6f191
Aleksa <kozicaleksa02@gmail.com>
2024-10-14 20:11:38 +0200

    Post entity class updated

--- src/Modules/Blog/Explorer.Blog.Core/Domain/Post.cs
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/Post.cs
@@ -1,27 +1,46 @@
 ﻿using Explorer.BuildingBlocks.Core.Domain;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Explorer.Blog.Core.Domain
 {
 
     public class Post : Entity
     {
-        public string Title { get; set; }
-        public long UserId { get; set; }
-        public string Description { get; set; }
-        public DateTime PublishDate { get; set; }
-        public string ImageUrl { get; set; }
-        public Status Status { get; set; }
+        public string Title { get; private set; }
+        public long UserId { get; private set; }
+        public string Description { get; private set; }
+        public DateTime PublishDate { get; private set; }
+        public string ImageUrl { get; private set; }
+        public Status Status { get; private set; }
+
+        public Post(string title , long userId , string description , DateTime publishDate , string imageUrl , Status status) { 
+            Title = title;
+            UserId = userId;
+            Description = description;
+            PublishDate = publishDate;
+            ImageUrl = imageUrl;
+            Status = status;
+            Validate();
+        }
+
+        private void Validate() {
+            if (UserId == 0) throw new ArgumentException("Invalid UserId");
+            if (string.IsNullOrWhiteSpace(Title)) throw new ArgumentException("Invalid Title");
+            if (string.IsNullOrEmpty(Status.ToString())) throw new ArgumentException("Invalid Status");
+            if (PublishDate == DateTime.MinValue && PublishDate < DateTime.Now) throw new ArgumentException("Invalid PublishDate");
+        }
 
     }
-}
 public enum Status
 {
     Draft,
     Published,
     Closed
+}
 }
