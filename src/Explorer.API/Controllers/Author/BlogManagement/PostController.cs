dffff3d03fb14012025d8a841d45013b2e720e8d
Aleksa <kozicaleksa02@gmail.com>
2024-10-14 21:18:43 +0200

    PostController created

---
+++ src/Explorer.API/Controllers/Author/BlogManagement/PostController.cs
@@ -0,0 +1,23 @@
+﻿using Explorer.Blog.API.Dtos;
+using Explorer.Blog.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Author.BlogManagement
+{
+    [Authorize(Policy = "administratorPolicy")]
+    [Route("api/blogManagement/post")]
+    public class PostController : BaseApiController
+    {
+        private readonly IPostService _postService;
+        public PostController(IPostService postService) {
+            _postService = postService;
+        }
+
+        [HttpPost]
+        public ActionResult<PostDto> Create([FromBody] PostDto post) {
+            var result = _postService.Create(post);
+            return CreateResponse(result);
+        }
+    }
+}
