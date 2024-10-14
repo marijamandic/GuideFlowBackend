a5a13d43fb92d8b3d0436a0dc7863546bc339a21
Mihailo84 <mihailo.vukovic2002@gmail.com>
2024-10-15 01:43:11 +0200

    Dodao kontroler za ocenjivanje aplikacije

---
+++ src/Explorer.API/Controllers/AppRatingController.cs
@@ -0,0 +1,26 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Microsoft.AspNetCore.Mvc.Route("api/tourist/AppRating")] // Koristite potpuni naziv
+    public class AppRatingController : BaseApiController
+    {
+        private readonly IRatingAppService _ratingAppService;
+
+        public AppRatingController(IRatingAppService ratingAppService)
+        {
+            _ratingAppService = ratingAppService;
+        }
+
+        [HttpPost("rating")]
+        public ActionResult<RatingAppDto> NewAppRating([FromBody] RatingAppDto ratingAppDto)
+        {
+            var result = _ratingAppService.Create(ratingAppDto);
+            return CreateResponse(result);
+        }
+    }
+}
