0f3045a92bd0b566b859522d6faeefe781af3d48
Nikola Ratkovac <nikolaratkovac3@gmail.com>
2024-10-14 21:03:01 +0200

    feat: updated dependency injection

---
+++ src/Explorer.API/Controllers/Author/TourObjectController.cs
@@ -0,0 +1,26 @@
+﻿using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Author
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/administration/tourObject")]
+    public class TourObjectController : BaseApiController
+    {
+        private readonly ITourObjectService _tourObjectService;
+
+        public TourObjectController(ITourObjectService tourObjectService)
+        {
+            _tourObjectService = tourObjectService;
+        }
+
+        [HttpPost]
+        public ActionResult<TourObjectDto> Create([FromBody] TourObjectDto tourObject)
+        {
+            var result = _tourObjectService.Create(tourObject);
+            return CreateResponse(result);
+        }
+    }
+}
