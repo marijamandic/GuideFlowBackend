0e5e3b545f48ea5664698978fa2a4b5554bebe59
Vasilije Kiso <kisovasilije46@gmail.com>
2024-10-15 12:19:13 +0200

    feat: add Problem, ProblemDto, IProblemService, ProblemService classes

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/ProblemDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos;
+
+public class ProblemDto
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int TourId { get; set; }
+    public string Category { get; set; }
+    public string Priority { get; set; }
+    public string Description { get; set; }
+    public DateOnly ReportedAt { get; set; }
+}
