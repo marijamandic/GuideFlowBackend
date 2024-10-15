d4e0c9369fb28844f3d9a6daaa06238cb377967e
kalabavukasin <149072466+kalabavukasin@users.noreply.github.com>
2024-10-15 10:59:24 +0200

    ClubService created

--- src/Modules/Tours/Explorer.Tours.Core/Domain/Club.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Club.cs
@@ -7,12 +7,23 @@ using System.Threading.Tasks;
 
 namespace Explorer.Tours.Core.Domain
 {
-    internal class Club : Entity
+    public class Club : Entity
     {
         public long ClubId { get; set; }
         public long OwnerId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+
+        public Club(long clubId,long ownerId,string name,string description,string imageUrl)
+        {
+            ClubId = clubId;
+            OwnerId = ownerId;
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+        }
     }
+    
+
 }
