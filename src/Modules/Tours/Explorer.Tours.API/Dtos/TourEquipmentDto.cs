81f592ec7306c531c7eeb555b5fb449ca1b70454
Kristina Todorovic <todorovic.kristina003@gmail.com>
2024-10-15 20:52:04 +0200

    feat: Created model, service and dto for TourEquipment

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourEquipmentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class TourEquipmentDto
+    {
+
+        public int TourId { get; set; }
+        public int EquipmentId { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourEquipment.cs
@@ -0,0 +1,23 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class TourEquipment : Entity
+    {
+        public int TourId { get; init; }
+        public int EquipmentId { get; init; }
+        public int Quantity { get; init; }
+
+        public TourEquipment( int tourId, int eqId)
+        {
+            TourId = tourId;
+            EquipmentId = eqId;
+            Quantity = 0;
+        }
+    }
+}
