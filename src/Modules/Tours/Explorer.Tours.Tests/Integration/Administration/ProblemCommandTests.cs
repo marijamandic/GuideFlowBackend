758e8c64b431847606d996f08da68a05f80c6302
Vasilije Kiso <kisovasilije46@gmail.com>
2024-10-15 21:05:46 +0200

    feat: add unit tests for fetching and creating problem

---
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/Administration/ProblemCommandTests.cs
@@ -0,0 +1,60 @@
+﻿using Explorer.API.Controllers.Tourist;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Explorer.Tours.Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Tests.Integration.Administration;
+
+[Collection("Sequential")]
+public class ProblemCommandTests : BaseToursIntegrationTest
+{
+    public ProblemCommandTests(ToursTestFactory factory) : base(factory) { }
+
+    [Fact]
+    public void Creates()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+        var dbContext = scope.ServiceProvider.GetRequiredService<ToursContext>();
+        var newEntity = new ProblemDto
+        {
+            UserId = 1,
+            TourId = 2,
+            Category = "testkat",
+            Priority = "testprior",
+            Description = "Description",
+            ReportedAt = DateOnly.FromDateTime(DateTime.Today)
+        };
+
+        // Act
+        var result = ((ObjectResult)controller.Create(newEntity).Result)?.Value as ProblemDto;
+
+        // Assert - response
+        result.ShouldNotBeNull();
+        result.Id.ShouldNotBe(0);
+        result.Category.ShouldBe(newEntity.Category);
+
+        // Assert - db
+        var storedEntity = dbContext.Problem.FirstOrDefault(p => p.Category == result.Category);
+        storedEntity.ShouldNotBeNull();
+        storedEntity.Id.ShouldBe(result.Id);
+    }
+
+    private static ProblemController CreateController(IServiceScope scope)
+    {
+        return new ProblemController(scope.ServiceProvider.GetRequiredService<IProblemService>())
+        {
+            ControllerContext = BuildContext("-1")
+        };
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/Administration/ProblemQueryTests.cs
@@ -0,0 +1,39 @@
+﻿using Explorer.API.Controllers.Administrator.Administration;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Tours.Tests.Integration.Administration;
+
+[Collection("Sequential")]
+public class ProblemQueryTests : BaseToursIntegrationTest
+{
+    public ProblemQueryTests(ToursTestFactory factory) : base(factory) { }
+
+    [Fact]
+    public void Retreives_all()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+
+        // Act
+        var result = ((ObjectResult)controller.GetAll().Result)?.Value as PagedResult<ProblemDto>;
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.Results.Count.ShouldBe(3);
+        result.TotalCount.ShouldBe(3);
+    }
+
+    private static ProblemController CreateController(IServiceScope scope)
+    {
+        return new ProblemController(scope.ServiceProvider.GetRequiredService<IProblemService>())
+        {
+            ControllerContext = BuildContext("-1")
+        };
+    }
+}
