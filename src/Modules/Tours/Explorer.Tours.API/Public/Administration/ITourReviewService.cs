bca7a2e81cd13314194b2480ce9e21895356563a
KornelijeKiso <kornelijekiso@gmail.com>
2024-10-17 00:30:37 +0200

    feat: tour reviews implemented

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/Administration/ITourReviewService.cs
@@ -0,0 +1,13 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using FluentResults;
+
+namespace Explorer.Tours.API.Public.Administration;
+
+public interface ITourReviewService
+{
+    Result<TourReviewDto> Get(int id);
+    Result<TourReviewDto> Create(TourReviewDto tourReview);
+    Result<TourReviewDto> Update(TourReviewDto tourReview);
+    Result Delete(int id);
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourReview.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain;
+public class TourReview : Entity
+{
+    public int Rating { get; init; }
+    public string? Comment { get; init; }
+    public DateTime TourDate { get; init; }
+    public DateTime CreationDate { get; init; }
+
+    public TourReview(int rating, string comment, DateTime tourDate, DateTime creationDate)
+    {
+        Rating = rating;
+        Comment = comment;
+        TourDate = tourDate;
+        CreationDate = creationDate;
+    }
+}
