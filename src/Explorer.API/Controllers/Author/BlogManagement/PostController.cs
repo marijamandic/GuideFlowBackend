01749cbee81d30743fe6185ec5706cde67a9ca41
Aleksa <kozicaleksa02@gmail.com>
2024-10-18 10:40:55 +0200

    feat: Implemented saving pictures to backend

--- src/Explorer.API/Controllers/Author/BlogManagement/PostController.cs
+++ src/Explorer.API/Controllers/Author/BlogManagement/PostController.cs
@@ -3,7 +3,7 @@ using Explorer.Blog.API.Public;
 using Explorer.BuildingBlocks.Core.UseCases;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using System.IO;
 namespace Explorer.API.Controllers.Author.BlogManagement
 {
     //[Authorize(Policy = "authorPolicy")]
@@ -11,8 +11,10 @@ namespace Explorer.API.Controllers.Author.BlogManagement
     public class PostController : BaseApiController
     {
         private readonly IPostService _postService;
-        public PostController(IPostService postService) {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public PostController(IPostService postService , IWebHostEnvironment webHostEnvironment) {
             _postService = postService;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         [HttpGet]
@@ -23,6 +25,23 @@ namespace Explorer.API.Controllers.Author.BlogManagement
 
         [HttpPost]
         public ActionResult<PostDto> Create([FromBody] PostDto post) {
+
+            if (!string.IsNullOrEmpty(post.ImageBase64)){
+                var imageData = Convert.FromBase64String(post.ImageBase64.Split(',')[1]);
+                var fileName = Guid.NewGuid() + ".png"; // ili bilo koji format slike
+                var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "images", "blogs");
+
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                var filePath = Path.Combine(folderPath, fileName);
+                System.IO.File.WriteAllBytes(filePath, imageData);
+                post.ImageUrl = $"images/blogs/{fileName}";
+            }
+
+
             var result = _postService.Create(post);
             return CreateResponse(result);
         }
