ec5fc4ab8b9b87f5f68c0a5644d74caee26c2d09
Miloslav Djendjur <djendjur.m@gmail.com>
2024-10-22 13:37:52 +0200

    feat: added some tests

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
@@ -1,4 +1,6 @@
 DELETE FROM stakeholders."People";
 DELETE FROM stakeholders."Users";
 DELETE FROM stakeholders."ClubRequests";
-DELETE FROM tours."Club";
+DELETE FROM stakeholders."Clubs";
+DELETE FROM stakeholders."ClubInvitations";
+DELETE FROM stakeholders."ClubMembers";
--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-clubRequests.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-clubRequests.sql
@@ -1,3 +1,19 @@
 ﻿INSERT INTO stakeholders."ClubRequests"(
 	"Id", "TouristId", "ClubId", "Status")
-VALUES (-3, 1, 1, 0);
+VALUES (1, 1, 1, 3);
+
+INSERT INTO stakeholders."ClubRequests"(
+	"Id", "TouristId", "ClubId", "Status")
+VALUES (2, 1, 1, 3);
+
+INSERT INTO stakeholders."ClubRequests"(
+	"Id", "TouristId", "ClubId", "Status")
+VALUES (3, 1, 1, 0);
+
+INSERT INTO stakeholders."ClubRequests"(
+	"Id", "TouristId", "ClubId", "Status")
+VALUES (4, 1, 1, 0);
+
+INSERT INTO stakeholders."ClubRequests"(
+	"Id", "TouristId", "ClubId", "Status")
+VALUES (5, 1, 1, 0);
