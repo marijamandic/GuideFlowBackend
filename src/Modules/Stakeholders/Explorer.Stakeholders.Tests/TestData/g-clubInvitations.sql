20c7bee3b53c4b50d30b5088997365e5397857ba
kalabavukasin <149072466+kalabavukasin@users.noreply.github.com>
2024-10-22 19:13:55 +0200

    feat: All tests are working now

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/g-clubInvitations.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO stakeholders."ClubInvitations"(
+	"Id", "ClubId", "TouristID", "Status")
+VALUES (-1, 19, 2, 0);
+INSERT INTO stakeholders."ClubInvitations"(
+	"Id", "ClubId", "TouristID", "Status")
+VALUES (-2, 10, 4, 0);
+INSERT INTO stakeholders."ClubInvitations"(
+	"Id", "ClubId", "TouristID", "Status")
+VALUES (-3, 20, 8, 0);
+INSERT INTO stakeholders."ClubInvitations"(
+	"Id", "ClubId", "TouristID", "Status")
+VALUES (-4, 21, 5, 0);
