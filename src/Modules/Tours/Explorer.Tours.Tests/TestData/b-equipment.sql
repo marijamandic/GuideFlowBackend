94255ef4fbd013aa6ab15e08a11f0cb6b56aa033
ra186-2021 <janicevicivan90@gmail.com>
2024-10-22 19:07:31 +0200

    fix: sql-equipment

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/b-equipment.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/b-equipment.sql
@@ -1,9 +1,9 @@
 ﻿INSERT INTO tours."Equipment"(
     "Id", "Name", "Description")
-VALUES (-1, 'Voda', 'Kolicina vode varira od temperature i trajanja ture. Preporuka je da se pije pola litre vode na jedan sat umerena fizicke aktivnosti (npr. hajk u prirodi bez znacajnog uspona) po umerenoj vrućini');
+VALUES (-1, 'Voda', 'Količina vode varira od temperature i trajanja ture. Preporuka je da se pije pola litre vode na jedan sat umerena fizičke aktivnosti (npr. hajk u prirodi bez značajnog uspona) po umerenoj vrućini');
 INSERT INTO tours."Equipment"(
     "Id", "Name", "Description")
-VALUES (-2, 'tapovi za etanje', 'tapovi umanjuju umor nogu, pospeuju aktivnost gornjeg dela tela i pruzaju stabilnost na neravnom terenu.');
+VALUES (-2, 'Štapovi za šetanje', 'Štapovi umanjuju umor nogu, pospešuju aktivnost gornjeg dela tela i pružaju stabilnost na neravnom terenu.');
 INSERT INTO tours."Equipment"(
     "Id", "Name", "Description")
-VALUES (-3, 'Obicna baterijska lampa', 'Baterijska lampa od 200 do 400 lumena.');
+VALUES (-3, 'Obična baterijska lampa', 'Baterijska lampa od 200 do 400 lumena.');
