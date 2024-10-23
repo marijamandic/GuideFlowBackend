74c2aca80c35df59d43c854ac0693240f8b35898
ra186-2021 <janicevicivan90@gmail.com>
2024-10-23 20:11:33 +0200

    add: Create & Edit - TourObject by map, longitude and latitude

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/b-checkpoint.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/b-checkpoint.sql
@@ -1,6 +1,6 @@
 ﻿INSERT INTO tours."Checkpoint"(
 	"Id", "Name", "Description", "Latitude", "Longitude", "ImageUrl", "TourId")
-	VALUES (-1, 'Main Entrance', 'Checkpoint at the main entrance', 45.2671, 19.8335, '/images/main-entrance.jpg', 0);
+	VALUES (-1, 'Main Entrance', 'Checkpoint at the main entrance', 45.2671, 19.8335, '/images/main-entrance.jpg', 1);
 INSERT INTO tours."Checkpoint"(
 	"Id", "Name", "Description", "Latitude", "Longitude", "ImageUrl", "TourId")
 	VALUES (-2, 'Restaurant Area', 'Checkpoint near the restaurant', 45.2682, 19.8356, '/images/restaurant-area.jpg', 1);
