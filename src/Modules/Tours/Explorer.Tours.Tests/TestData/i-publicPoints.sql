cf5f4c55007b787f4957a9e9d6586e7327647609
ra186-2021 <janicevicivan90@gmail.com>
2024-11-13 11:48:00 +0100

    fix: Tour test

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/i-publicPoints.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/i-publicPoints.sql
@@ -1,14 +1,14 @@
 ﻿INSERT INTO tours."PublicPoints"(
-    "Id", "Name", "Description", "Latitude", "Longitude", "ImageUrl", "ApprovalStatus", "PointType")
+    "Id", "Name", "Description", "Latitude", "Longitude", "ImageUrl", "ApprovalStatus", "PointType", "AuthorId")
 VALUES
-(-1, 'Turski Vrh', 'Prelep pogled sa vrha, idealan za fotografisanje.', 44.8176, 20.4633, '/images/turski-vrh.jpg', 0, 0);
+(-1, 'Turski Vrh', 'Prelep pogled sa vrha, idealan za fotografisanje.', 44.8176, 20.4633, '/images/turski-vrh.jpg', 0, 0, -11);
 
 INSERT INTO tours."PublicPoints"(
-    "Id", "Name", "Description", "Latitude", "Longitude", "ImageUrl", "ApprovalStatus", "PointType")
+    "Id", "Name", "Description", "Latitude", "Longitude", "ImageUrl", "ApprovalStatus", "PointType", "AuthorId")
 VALUES
-(-2, 'Stara Crkva', 'Historijska crkva iz 19. veka, mesto mirnog odmora.', 44.8230, 20.4680, '/images/stara-crkva.jpg', 1, 1);
+(-2, 'Stara Crkva', 'Historijska crkva iz 19. veka, mesto mirnog odmora.', 44.8230, 20.4680, '/images/stara-crkva.jpg', 1, 1, -11);
 
 INSERT INTO tours."PublicPoints"(
-    "Id", "Name", "Description", "Latitude", "Longitude", "ImageUrl", "ApprovalStatus", "PointType")
+    "Id", "Name", "Description", "Latitude", "Longitude", "ImageUrl", "ApprovalStatus", "PointType", "AuthorId")
 VALUES
-(-3, 'Izvor Čiste Vode', 'Mesto gde možete napuniti vodu, prirodni izvor.', 44.8100, 20.4800, '/images/izvor-vode.jpg', 0, 0);
+(-3, 'Izvor Čiste Vode', 'Mesto gde možete napuniti vodu, prirodni izvor.', 44.8100, 20.4800, '/images/izvor-vode.jpg', 0, 0, -11);
