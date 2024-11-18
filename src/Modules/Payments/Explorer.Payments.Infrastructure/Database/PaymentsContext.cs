833daa871749463debfee3a04156c32dd07368d8
kisovasilije <148854727+kisovasilije@users.noreply.github.com>
2024-11-18 11:35:43 +0100

    Revert "Feat/shopping cart"

--- src/Modules/Payments/Explorer.Payments.Infrastructure/Database/PaymentsContext.cs
+++ src/Modules/Payments/Explorer.Payments.Infrastructure/Database/PaymentsContext.cs
@@ -1,28 +1,13 @@
-﻿using Explorer.Payments.Core.Domain.ShoppingCarts;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Explorer.Payments.Infrastructure.Database;
 
 public class PaymentsContext : DbContext
 {
-    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
-    public DbSet<SingleItem> SingleItems { get; set; }
-
     public PaymentsContext(DbContextOptions<PaymentsContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("payments");
-
-        ConfigureShoppingCart(modelBuilder);
-    }
-
-    private static void ConfigureShoppingCart(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<ShoppingCart>()
-            .HasMany(sc => sc.SingleItems)
-            .WithOne()
-            .HasForeignKey(si => si.ShoppingCartId)
-            .OnDelete(DeleteBehavior.Cascade);
     }
 }
