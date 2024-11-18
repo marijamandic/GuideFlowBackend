a6d4b7dcb82cd64f928ca456985f45f6e56df522
Vasilije Kiso <kisovasilije46@gmail.com>
2024-11-18 11:31:36 +0100

    fix: payments migrations

--- src/Modules/Payments/Explorer.Payments.Infrastructure/Database/PaymentsContext.cs
+++ src/Modules/Payments/Explorer.Payments.Infrastructure/Database/PaymentsContext.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Explorer.Payments.Core.Domain.ShoppingCarts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Explorer.Payments.Infrastructure.Database;
 
 public class PaymentsContext : DbContext
 {
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<SingleItem> SingleItems { get; set; }
+
     public PaymentsContext(DbContextOptions<PaymentsContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("payments");
+
+        ConfigureShoppingCart(modelBuilder);
+    }
+
+    private static void ConfigureShoppingCart(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ShoppingCart>()
+            .HasMany(sc => sc.SingleItems)
+            .WithOne()
+            .HasForeignKey(si => si.ShoppingCartId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
