20d2a9ff48e153afa87d4aa6e87521cd7a0ac595
Kristina Todorovic <todorovic.kristina003@gmail.com>
2024-11-24 15:17:10 +0100

    feat: crud methods implemented

--- src/Modules/Encounters/Explorer.Encounters.API/Public/IEncounterExecutionService.cs
+++ src/Modules/Encounters/Explorer.Encounters.API/Public/IEncounterExecutionService.cs
@@ -10,10 +10,10 @@ using System.Threading.Tasks;
 
 namespace Explorer.Encounters.API.Public
 {
-    public class IEncounterExecutionService
+    public interface IEncounterExecutionService
     {
-        public Result<EncounterExecutionDto> Create(EncounterDto encounterDto);
-        public Result<EncounterExecutionDto> Update(EncounterDto encounterDto);
+        public Result<EncounterExecutionDto> Create(EncounterExecutionDto encounterExecutionDto);
+        public Result<EncounterExecutionDto> Update(EncounterExecutionDto encounterExecutionDto);
         public Result Delete(int id);
         public Result<EncounterExecutionDto> Get(long id);
         public Result<PagedResult<EncounterExecutionDto>> GetPaged(int page, int pageSize);
--- src/Modules/Encounters/Explorer.Encounters.Core/UseCases/EncounterExecutionService.cs
+++ src/Modules/Encounters/Explorer.Encounters.Core/UseCases/EncounterExecutionService.cs
@@ -1,6 +1,9 @@
-﻿using Explorer.BuildingBlocks.Core.UseCases;
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Encounters.API.Dtos;
 using Explorer.Encounters.API.Public;
+using Explorer.Encounters.Core.Domain;
+using Explorer.Encounters.Core.Domain.RepositoryInterfaces;
 using FluentResults;
 using System;
 using System.Collections.Generic;
@@ -10,19 +13,54 @@ using System.Threading.Tasks;
 
 namespace Explorer.Encounters.Core.UseCases
 {
-    public class EncounterExecutionService :IEncounterExecutionService
+    public class EncounterExecutionService : BaseService<EncounterExecutionDto, EncounterExecution>, IEncounterExecutionService
     {
-        public Result<EncounterExecutionDto> Create(EncounterDto encounterDto)
+        private readonly IEncounterExecutionRepository _encounterExecutionRepository;
+        public EncounterExecutionService(IEncounterExecutionRepository encounterExecutionRepository, IMapper mapper) : base(mapper)
         {
-            throw new NotImplementedException();
+            _encounterExecutionRepository = encounterExecutionRepository;
         }
-        public Result<EncounterExecutionDto> Update(EncounterDto encounterDto)
-        {
-            throw new NotImplementedException();
-        }
-        public Result Delete(int id) { throw new NotImplementedException(); }
 
-        public Result<EncounterExecutionDto> Get(long id) { throw new NotImplementedException(); }
-        public Result<PagedResult<EncounterExecutionDto>> GetPaged(int page, int pageSize) { throw new NotImplementedException(); }
+        public Result<EncounterExecutionDto> Create(EncounterExecutionDto encounterExecutionDto)
+        {
+
+            //ako postoji ecounteer sa tim id-jem  i nije complete prebaci na pridruzi 
+            throw new NotImplementedException();
+        }
+        public Result<EncounterExecutionDto> Update(EncounterExecutionDto encounterExecutionDto)
+        {
+            var encounter = _encounterExecutionRepository.Update(MapToDomain(encounterExecutionDto));
+            return MapToDto(encounter);
+
+        }
+        public Result Delete(int id) {
+
+            try
+            {
+                _encounterExecutionRepository.Delete(id);
+                return Result.Ok();
+            }
+            catch (KeyNotFoundException e)
+            {
+                return Result.Fail(FailureCode.NotFound).WithError(e.Message);
+            }
+            
+        }
+
+        public Result<EncounterExecutionDto> Get(long id)
+        {
+            var encounterExecution= _encounterExecutionRepository.Get(id);
+            if (encounterExecution is null)
+                return Result.Fail(FailureCode.NotFound);
+            return MapToDto(encounterExecution);
+        }
+        public Result<PagedResult<EncounterExecutionDto>> GetPaged(int page, int pageSize) {
+            var encountersExecutions = _encounterExecutionRepository.GetPaged(page, pageSize);
+            return MapToDto(encountersExecutions);
+        }
+    
+       
+     
+    
     }
 }
