78b1569d656ca2baafe65833c5a6cb974e385bf6
dimitrijesunka12 <dimitrijesunka99@gmail.com>
2024-11-24 12:48:19 +0100

    dodao tourist logiku u context

--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/StakeholdersContext.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/StakeholdersContext.cs
@@ -20,6 +20,7 @@ public class StakeholdersContext : DbContext
     public DbSet<AppRating> Ratings { get; set; }
     public DbSet<Notification> Notifications { get; set; }
     public DbSet<ProblemNotification> ProblemNotifications { get; set; }
+    public DbSet<Tourist> Tourists {  get; set; }
 
     public StakeholdersContext(DbContextOptions<StakeholdersContext> options) : base(options) { }
 
@@ -56,6 +57,10 @@ public class StakeholdersContext : DbContext
             .HasOne<User>()
             .WithOne()
             .HasForeignKey<ProfileInfo>(s => s.UserId);
+
+        modelBuilder.Entity<Tourist>()
+            .ToTable("Tourists")
+            .HasBaseType<User>();
     }
 
     private static void ConfigureClubInvitation(ModelBuilder modelBuilder)
