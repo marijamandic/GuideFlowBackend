44060dfffe21de2b19ffe5c55bb40c43ad2a8af5
Aleksa Curaković <aleksa.curakovic@gmail.com>
2024-11-25 18:34:40 +0100

    Feat: Added TourBundle class and DTO

---
+++ src/Modules/Payments/Explorer.Payments.API/Dtos/TourBundleDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.API.Dtos
+{
+    public class TourBundleDto
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public Status Status { get; set; }
+
+        public List<long> TourIds { get; set; } = new List<long>();
+        
+    }
+    public enum Status
+    {
+        Draft,
+        Published,
+        Archieved
+    }
+}
---
+++ src/Modules/Payments/Explorer.Payments.Core/Domain/TourBundle.cs
@@ -0,0 +1,54 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Core.Domain
+{
+    public  class TourBundle: Entity
+    {
+        public string Name { get; private set; }
+
+        public double Price { get; private set; }
+
+        public Status Status { get; private set; }
+
+        private List<long> _tourIds = new List<long>();
+
+        public IReadOnlyList<long> TourIds => new List<long>(_tourIds);
+
+        public TourBundle(string name, double price, Status status) 
+        {
+            Name = name;
+            Price = price;
+            Status = status;
+            Validate();
+        }
+
+        public void AddTour(long tourId) 
+        {
+            _tourIds.Add(tourId);
+        }
+
+        public void RemoveTour(long tourId)
+        {
+            if (_tourIds.Count == 0) throw new ArgumentException("Cannot Remove Anymore Tours");
+            _tourIds.Remove(tourId);
+        }
+
+        public void Validate()
+        {
+            if (Price <= 0) throw new ArgumentException("Invalid Bundle Price");
+            if (!Enum.IsDefined(typeof(Status), Status)) throw new ArgumentException("Invalid Bundle Status");
+        }
+    }   
+
+    public enum Status
+    {
+        Draft,
+        Published,
+        Archieved
+    }
+}
