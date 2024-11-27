0b1fe2da1579f148a5068b3e19c5338187edc582
dimitrijesunka12 <dimitrijesunka99@gmail.com>
2024-11-27 16:42:10 +0100

    added id in bundle dto

--- src/Modules/Payments/Explorer.Payments.API/Dtos/TourBundleDto.cs
+++ src/Modules/Payments/Explorer.Payments.API/Dtos/TourBundleDto.cs
@@ -8,6 +8,7 @@ namespace Explorer.Payments.API.Dtos
 {
     public class TourBundleDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
 
         public double Price { get; set; }
