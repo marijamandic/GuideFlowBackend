114b6240caac9b1a70530f6b719cdc927b50596c
Vasilije Kiso <kisovasilije46@gmail.com>
2024-12-09 17:53:47 +0100

    fix: add DI for iinternaltourservice

--- src/Modules/Tours/Explorer.Tours.API/Dtos/TourDetailsDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Explorer.Tours.API.Dtos;
-
-public class TourDetailsDto
-{
-    public int Id { get; set; }
-    public required string Description { get; set; }
-    public Level Level { get; set; }
-    public List<string> Tags { get; set; } = new();
-}
--- src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
@@ -54,6 +54,7 @@ public static class ToursStartup
         services.AddScoped<IInternalProblemService, InternalProblemService>();
         services.AddScoped<IPublicPointService, PublicPointService>();
         services.AddScoped<IInternalSalesService, InternalSalesService>();
+        services.AddScoped<IInternalTourService, TourService>();
 
     }
 
